cdf2d70569d2209888ffebf95cd501017f6032a7
Ushank Radadiya <ushank@getlokalapp.com>
2022-03-05 16:51:35 +0530

    .

--- SamparkSetu/settings.py
+++ SamparkSetu/settings.py
@@ -152,7 +152,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, 'Yuvak\static'),
+    os.path.join(BASE_DIR, 'Yuvak/static'),
 )
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
